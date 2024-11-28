1223c61549186fef1214176cc9b09cb363cf06a5
Tom Quez <tom.quez@live.fr>
2024-11-28 09:33:50 +0100

    Update init env script

--- init_env.sh
+++ init_env.sh
@@ -6,7 +6,7 @@ eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 if [[ "$CONDA_DEFAULT_ENV" == "base" ]] ; then
     conda deactivate
 fi
-
+# replace the env name "benchmarking" with your own env name
 conda activate benchmarking
 
 echo "Environment variables set up"
